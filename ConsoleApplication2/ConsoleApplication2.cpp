234abca39fac385b1307d4094d3b849ddc9fa5ec
brryan12 <bryancr1208@gmail.com>
2024-09-19 16:26:56 -0600

    a

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,8 +6,9 @@ int main()
     Perro *p1=new Perro("Aleman", "Juan", "Perro","4545",45.6);
     std::cout<<p1->toString();
     std::endl(std::cout);
-    Gato* g1 = new Gato(15, "Michi", "Naranjado", "4541", 15.6);
+    Gato* g1 = new Gato(15, "Michi", "Gato", "4541", 15.6);
     std::cout<<g1->toString();
 
+    return 0;
 }
 

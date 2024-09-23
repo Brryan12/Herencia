e74955b51295f6aae00b851f128562eaccfa7a23
brryan12 <bryancr1208@gmail.com>
2024-09-23 16:08:16 -0600

    vector animales

---
+++ ConsoleApplication2/ContenedorAnimales.cpp
@@ -0,0 +1,1 @@
+#include "ContenedorAnimales.h"
--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -4,13 +4,14 @@
 #include "Caballo.h"
 int main()
 {
-    Perro *p1=new Perro("Aleman", "Juan", "Perro","4545",45.6);
+    //Al ser todos tipo Animal pueden ser guardadas en un mismo vector o lista
+    Animal *p1=new Perro("Aleman", "Juan", "Perro","4545",45.6);
     std::cout<<p1->toString();
     std::endl(std::cout);
-    Gato* g1 = new Gato(15, "Michi", "Gato", "4541", 15.6);
+    Animal* g1 = new Gato(15, "Michi", "Gato", "4541", 15.6);
     std::cout<<g1->toString();
     std::endl(std::cout);
-    Caballo* c1 = new Caballo(12.5, "Juan", "Pura", "215", 45.6);
+    Animal* c1 = new Caballo(12.5, "Juan", "Pura", "215", 45.6);
     std::cout << c1->toString();
 
     std::endl(std::cout);
@@ -20,6 +21,22 @@ int main()
     g1->felizCumpleanos();
     p1->felizCumpleanos();
 
+    Animal* vec[10];
+    int cant = 3;
+    for (int i = 0; i < 3; i++)
+        vec[i] = nullptr;
+
+    //ingresarlos al vector
+    vec[0] = g1;
+    vec[1] = c1;
+    vec[2] = p1;
+    system("pause");
+    system("cls");
+    std::cout << "Vector de animales" << std::endl;
+
+    for (int i = 0; i < cant; i++) {
+        std::cout << vec[i]->toString() << std::endl;
+    }
     delete c1;
     delete g1;
     delete p1;
